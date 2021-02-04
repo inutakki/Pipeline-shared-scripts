2377d8bd8734e76533cef992c25557929a0e8c81
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-04 07:44:05 -0800

    updated

--- vars/schemaValidatorTest.groovy
+++ vars/schemaValidatorTest.groovy
@@ -1,6 +1,11 @@
 def call(){
-    def validatorFile = libraryResource 'openApiSChema/validatorTest.js'
-        writeFile file: "validatorTest.js", text: validatorFile
 
+    def package = libraryResource 'openApiSChema/package.js'
+       writeFile file: "package.json" text: package
+    def zschemaValidator = libraryResource 'openApiSChema/zschemaValidator.js'
+       writeFile file: "zschemaValidator.js" text: package  
+    def validatorTest = libraryResource 'openApiSChema/validatorTest.js'
+        writeFile file: "validatorTest.js", text: validatorTest
+    sh "npm install";
     sh(returnStdout: true, script: "node validatorTest.js").trim();
 }
