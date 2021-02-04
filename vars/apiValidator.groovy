1430638b71213bf41591262a20d9eeca6e7db76f
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-04 12:51:37 -0800

    updated

--- vars/apiValidator.groovy
+++ vars/apiValidator.groovy
@@ -7,8 +7,8 @@ def call(def apiFile){
     def validatorTest = libraryResource 'openApiSChema/validatorTest.js'
         writeFile file: "validatorTest.js", text: validatorTest
     sh "npm install";
-    sh "node validatorTest.js ${apiFile} > testResults"
-	console.log("testResults: ${testResults}"
+    //sh "node validatorTest.js ${apiFile} > testResults"
+	//console.log("testResults: ${testResults}"
     def apiResult  = sh(returnStdout: true, script: "node validatorTest.js ${apiFile}").split("\r?\n")    
-return apiResult;
+return apiResult[0];
 }
