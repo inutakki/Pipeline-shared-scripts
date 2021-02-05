cc3491cb9ede3fb48d4e2473d8974175af24303b
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-04 21:09:18 -0800

    updated:

--- resources/openApiSChema/validatorTest.js
+++ resources/openApiSChema/validatorTest.js
@@ -5,22 +5,14 @@ const { openapi } = require("openapi-schemas");
 const yaml = require("js-yaml");
 const fs = require('fs');
 const path = require("path");
-const schemaValidator = require("./zschemaValidator.js")
+const validator1 = require("./zschemaValidator.js")
 const SwaggerParser = require("swagger-parser");
 
-let apiFile = "./definitions/swagger.yml"
-if(process.argv[2] != null){
-    
-    apiFile = process.argv[2]
-    console.log("apiFile: ${apiFile}")
-}
-let commitId;
-if(process.argv[3] != null){
-    commitId = process.argv[3];
-}
+const apiFile = "./definitions/swagger.yaml"
 const isConfig = false;
 
-async function validate (apiFile) {
+async function validate (apiFile, isConfig) {
+    //console.log("inside Function");
     if (!(fs.existsSync(apiFile, 'utf8'))) {
         throw Error(`api spec doc file does not exist: ${apiFile}`)
     }
@@ -33,27 +25,29 @@ async function validate (apiFile) {
         schema = SwaggerParser.YAML.parse(schemaObject);
     } else {
         try {
+            //apiJSON = await parser(apiFile);
             apiJSON = await SwaggerParser.parse(apiFile);
             console.log("API name: %s, Version: %s, Type: %s", apiJSON.info.title, apiJSON.info.version, (apiJSON.openapi ? `openapi ${apiJSON.openapi}` : 'swagger 2.0' ));
         } catch (e) {
             console.log(e);
         }
     }
-const a =  schemaValidator(apiJSON, null);
+const a =  validator1(apiJSON, null);
+//console.log(`updated ${a}`);
 if(a){
    const result =  JSON.stringify({"validated": `${a}`,
     "DODItem": "OpenAPISchemaValidation",
     "Description": "Validates API specification with open API SChema",
     "API name": apiJSON.info.title,
     "squad": "undefined",
-    "commitID": `${commitId}`,
+    "commitID": process.argv[3],
     "status": "Passed",
-    "message": `${apiJSON.info.title}  validated with open API schema for ${commitId}`
+    "message": `${apiJSON.info.title}` + " validated with open API schema for commitID"
 
     } )
     console.log(result);
     return result;
     }
 }
-//console.log(process.argv[2]);
- return validate(apiFile);
+console.log(process.argv[2]);
+ return validate(apiFile, false);
--- vars/apiValidator.groovy
+++ vars/apiValidator.groovy
@@ -7,16 +7,13 @@ def call(def apiFile = "./definitions/swagger.yml"){
        writeFile file: "zschemaValidator.js", text: zschemaValidator  
     def validatorTest = libraryResource 'openApiSChema/validatorTest.js'
         writeFile file: "validatorTest.js", text: validatorTest
-    def commitHash = sh (returnStdout: true, script:"git log -n 1 --pretty=format:'%H'")
-    
-    def apiResult;
-    dir("${pwd}"){
-        sh "npm install";
-         apiResult  = sh(returnStdout: true, script: "node validatorTest.js ${apiFile} ${commitHash}").split("\r?\n")
-         def jsonResult = new JsonSlurperClassic().parseText(apiResult[apiResult.length -1])
-        println("Inside Groovy function: "+ jsonResult.validated)    
-    }
-    
+        def commitHash = sh (returnStdout: true, script:"git log -n 1 --pretty=format:'%H'")
+    sh "npm install";
+    def apiResult  = sh(returnStdout: true, script: "node validatorTest.js ${apiFile} ${commitHash}").split("\r?\n")    
+       println("result: " + apiResult);
+       // println("result: " + result1);
+       def jsonResult = new JsonSlurperClassic().parseText(apiResult[apiResult.length -1])
+     println("Inside Groovy function: "+ jsonResult.validated)
 
 return apiResult[apiResult.length -1];
 }
