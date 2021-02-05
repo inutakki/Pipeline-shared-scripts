f37338a3b3af8ac10ff875aa3a4e2340103db9de
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-04 19:58:12 -0800

    added

--- resources/openApiSChema/validatorTest.js
+++ resources/openApiSChema/validatorTest.js
@@ -12,7 +12,7 @@ const { Console } = require("console");
 
 
 const apiFile = process.argv[2]
-const commitId = process.argv[3]
+//const commitId = process.argv[3]
 
 const isConfig = false;
 
@@ -37,16 +37,16 @@ async function validate (apiFile, isConfig) {
         }
     }
     let isValid =  ZschemaValidator(apiJSON);
-    console.log(isValid);
+    //console.log(isValid);
     if(isValid){
         const result =  JSON.stringify({ "validated": isValid,
         "DODItem": "OpenAPISchemaValidation",
         "Description": "Validates API specification with open API SChema",
         "API name": apiJSON.info.title,
         "squad": "undefined",
-        "commitID": commitId,
+        "commitID": "commitId",
         "status": "Passed",
-        "message": `${apiJSON.info.title} successfully validated with open API schema for ${commitId}`
+        "message": `${apiJSON.info.title} successfully validated with open API schema for commitId`
         })
         return result;
     }
