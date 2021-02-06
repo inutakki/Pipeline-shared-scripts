a93f79e5cd65a54d21024222a26f320c29731f40
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-05 16:11:42 -0800

    updated

---
+++ vars/apiValidatorSh.groovy
@@ -0,0 +1,28 @@
+import groovy.json.JsonSlurperClassic
+def call(def apiFile = "./definitions/swagger.yml"){
+
+    def test = libraryResource 'openApiSChema/package.json'
+       writeFile file: "precommit-dod/package.json", text: test
+    def zschemaValidator = libraryResource 'openApiSChema/zschemaValidator.js'
+       writeFile file: "precommit-dod/zschemaValidator.js", text: zschemaValidator  
+    def validatorTest = libraryResource 'openApiSChema/validatorTest.js'
+        writeFile file: "precommit-dod/validatorTest.js", text: validatorTest
+        def commitHash = sh (returnStdout: true, script:"git log -n 1 --pretty=format:'%H'")
+    def apiResult
+    try{
+
+        sh "cd precommit-dod; npm install";
+       
+       
+       sh "node precommit-dod/validatorTest.js ${apiFile} ${commitHash} "
+        /*def a = readJson text: testResult;
+        println("${a}");*/
+    } catch(error){
+        
+        println(error)
+        throw error
+        //currentBuild.result = 'FAILED'
+    }
+
+//return apiResult[apiResult.length -1];
+}
