fed5edfc4899041d816a5a310678fa880aaf9377
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-07 14:22:45 -0800

    updated

--- vars/apiValidatorSh.groovy
+++ vars/apiValidatorSh.groovy
@@ -15,8 +15,8 @@ def call(def apiFile = "./definitions/swagger.yml"){
        def testResult;
        
        sh "node precommit-dod/validatorTest.js ${apiFile} ${commitHash} > testResult"
-        //def a = readJson text: testResult;
-        println("${testResult}");
+        def a = readJson text: testResult;
+        println("${a}");
     } catch(error){
         
         println(error)
