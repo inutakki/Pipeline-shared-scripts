2ca4d56deeca19140ce76dd56fc6eaba59e8cb94
Indira Nutakki <indiranutakki@IS-5032s-MacBook-Pro.local>
2021-02-08 09:56:00 -0800

    updated

--- resources/openApiSChema/validatorTest.js
+++ resources/openApiSChema/validatorTest.js
@@ -30,7 +30,8 @@ async function validate (apiFile, isConfig) {
     } else {
         try {
             apiJSON = await SwaggerParser.parse(apiFile);
-            shell.exec("echo API name: $apiJSON.info.title, Version: $apiJSON.info.version, Type: (${apiJSON.openapi} ? openapi ${apiJSON.openapi} : 'swagger 2.0' )");
+            type = apiJSON.openapi ? `openapi ${apiJSON.openapi}` : 'swagger 2.0' ;
+            shell.exec("echo API name: $apiJSON.info.title, Version: $apiJSON.info.version, Type: $type");
         } catch (e) {
             console.log(e);
             throw e;
