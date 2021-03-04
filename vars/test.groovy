136dee813c728618010248e63049112fff29f68b
inutakki <inutakki@yahoo.com>
2021-03-03 22:51:16 -0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -10,6 +10,9 @@ import com.cloudbees.groovy.cps.NonCPS
 		def causeDescription
 		for ( buildCause in buildCauses ) {
 			if (buildCause != null) {
+				if( buildCause instanceof hudson.model.Cause$UserIdCause){
+					println("triggered by user")
+				}
 				causeDescription = buildCause.getShortDescription()
 				if (causeDescription.contains("Started by timer")) {
 					startedByTimer = true
