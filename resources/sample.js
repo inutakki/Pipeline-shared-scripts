4291ef7d2cf791fd5776c2ed185ddec65c241ce5
inutakki <inutakki@yahoo.com>
2021-03-09 07:42:29 -0800

    Update sample.js

--- resources/sample.js
+++ resources/sample.js
@@ -2,7 +2,7 @@ const jsonargs = `${process.argv[2]}`
 
 function getJsonArgs(jsonargs){
   
-  let temp = json.stringfy(json.parse(jsonargs))
+  let temp = JSON.parse(JSON.stringify(jsonargs))
   console.log(temp.buildId)
   
 }
