f2af66483d7005b99728726bbad1f4ca32a42d6b
inutakki <inutakki@yahoo.com>
2021-03-09 07:38:15 -0800

    Update getjsonArguments.groovy

--- vars/getjsonArguments.groovy
+++ vars/getjsonArguments.groovy
@@ -7,6 +7,8 @@ def call(){
   def jsonProps = JsonOutput.toJson(["json_Props": [buildProps]])
   println("class in groovy"+ jsonProps.getClass())
   println("jsonProps: " + jsonProps)
+   def test = libraryResource 'sample.json'
+   writeFile file: "sample.js", text: test
   def jsOutput =  sh(returnStdout: true, script: "node sample.js ${jsonProps}").split("\r?\n") 
   println(jsOutput)
   
