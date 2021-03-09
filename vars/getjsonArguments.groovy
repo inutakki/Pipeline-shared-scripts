988e1843ef6b0c6e8deebdac33a67aa562fea7a8
inutakki <inutakki@yahoo.com>
2021-03-09 07:48:05 -0800

    Update getjsonArguments.groovy

--- vars/getjsonArguments.groovy
+++ vars/getjsonArguments.groovy
@@ -4,7 +4,8 @@ import groovy.json.JsonBuilder
 def call(){
    def buildProps = [:]
   buildProps.put("buildNumber","${env.BUILD_NUMBER}")
-  def jsonProps = JsonOutput.toJson(["json_Props": [buildProps]])
+  //def jsonProps = JsonOutput.toJson(["json_Props": [buildProps]])
+   def jsonProps = JsonOutput.toJson([buildProps)
   println("class in groovy"+ jsonProps.getClass())
    println("jsonProps: ${jsonProps}")
    def test = libraryResource 'sample.js'
